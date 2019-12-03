5c0a7396dd3ecef28c76a24f7c10e957685ca35d
becs <becs@storypark.com>
2019-12-04 11:49:24 +1300

    Day One

---
+++ day_one/day_one.rb
@@ -0,0 +1,30 @@
+inputs = File.open("inputs.txt").read.split("\n").map(&:to_i)
+$each_input = []
+$sum = []
+
+def start(input)
+  $each_input << fuel_counter_upperer(input)
+  fuel_check($each_input.last)
+  each_fuel_total
+end
+
+def fuel_check(fuel)
+  if (fuel_counter_upperer(fuel) > 0)
+    $each_input << fuel_counter_upperer(fuel)
+    fuel_check($each_input.last)
+  end
+end
+
+def fuel_counter_upperer(mass)
+  mass / 3.floor - 2
+end
+
+def each_fuel_total
+  sum = $each_input.sum
+  $sum << sum
+  $each_input = []
+end
+
+inputs.map{ |i| start(i) }
+
+puts $sum.sum
