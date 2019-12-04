498cc1b4ff26e53e56252a118089668f5db293ee
becs <becs@storypark.com>
2019-12-04 20:44:56 +1300

    Day Two Part Two

--- challenges/day_two.rb
+++ challenges/day_two.rb
@@ -1,5 +1,7 @@
-$input = File.open("day_two_input.txt").read.split(",").map(&:to_i)
+$original = File.open("day_two_input.txt").read.split(",").map(&:to_i)
 $idx = 0
+$solution = 0
+$input = 0
 
 def find_addends(opcode)
   if opcode != 99
@@ -20,7 +22,23 @@ def opcode(code, d_one, d_two, position)
 end
 
 def terminate
-  print $input
+  $solution = $input.first
 end
 
-find_addends($input.first)
+def instruction
+  noun_nums = Array(1..80)
+  verb_nums = Array(1..80)
+
+  for n in noun_nums
+    for v in verb_nums
+      $input = File.open("day_two_input.txt").read.split(",").map(&:to_i)
+      $idx = 0
+      $input[1] = n
+      $input[2] = v
+      find_addends($input.first)
+      puts 100 * n + v if $solution == 19690720
+    end
+  end
+end
+
+instruction
